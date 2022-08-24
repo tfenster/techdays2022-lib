ce7116977cb79d0e0976a616ff5133532666ea17
Tobias Fenster <tfenster@4psbau.de>
2022-08-24 18:31:24 +0000

    init

---
+++ TechConferenceLib.cs
@@ -0,0 +1,11 @@
+﻿namespace techdays2022_lib;
+public class TechConferenceLib
+{
+    public string GetBCTechConferenceRating(string conferenceName)
+    {
+        if (string.Equals("BC TechDays", conferenceName, StringComparison.InvariantCultureIgnoreCase))
+            return "awesome";
+        else
+            return "meh";
+    }
+}
